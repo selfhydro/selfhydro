77b451cf18a47b661ad252dcc9edda52ba26ad27
bchalk101 <boruch11@gmail.com>
2018-03-02 11:00:30 +1100

    Remove cross compile env for cgo

--- .ci/build-linux-arm.sh
+++ .ci/build-linux-arm.sh
@@ -11,7 +11,6 @@ GO_CROSS_ENV=(
 	GOARM=7
 	GOOS=linux
 	CGO_ENABLED=1
-	CC=arm-linux-gnueabihf-gcc
 )
 
 OUTPUT_DIR=$(pwd)/linux-arm-binary
@@ -19,11 +18,6 @@ OUTPUT_DIR=$(pwd)/linux-arm-binary
 export GOPATH=$(pwd)/gopath:$(pwd)/gopath/src/github.com/bchalk101/selfhydro/Godeps/_workspace
 cd gopath/src/github.com/bchalk101/selfhydro
 
-if which arm-linux-gnueabihf-gcc 1> /dev/null 2>&1; then
-  env go get ./...
-  env ${GO_CROSS_ENV[@]} go build -o "$OUTPUT_DIR/self-hydro"
-  env ${GO_ENV[@]} go test ./... --cover -v
-else
-  echo "Cross-compile environment is not installed."
-  exit 1
-fi
+env go get ./...
+env ${GO_CROSS_ENV[@]} go build -o "$OUTPUT_DIR/self-hydro"
+env ${GO_ENV[@]} go test ./... --cover -v
