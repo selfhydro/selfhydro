6c5fb47645c93e4a7d17fde8436388616cd42b0f
bchalk101 <boruch11@gmail.com>
2018-03-02 14:53:45 +1100

    Change go get

--- .ci/build-linux-arm.sh
+++ .ci/build-linux-arm.sh
@@ -18,6 +18,6 @@ OUTPUT_DIR=$(pwd)/linux-arm-binary
 export GOPATH=$(pwd)/gopath:$(pwd)/gopath/src/github.com/bchalk101/selfhydro/Godeps/_workspace
 cd gopath/src/github.com/bchalk101/selfhydro
 
-env go get ./...
-env ${GO_CROSS_ENV[@]} go build -o "$OUTPUT_DIR/self-hydro"
+env go get
+env ${GO_CROSS_ENV[@]} go build -o "$OUTPUT_DIR/selfhydro"
 env ${GO_ENV[@]} go test ./... --cover -v
