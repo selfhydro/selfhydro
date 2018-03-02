c8c6ac90c8508a8f685ceada4428bcce4cf217e7
bchalk101 <boruch11@gmail.com>
2018-03-02 15:04:32 +1100

    Change version of golang in docker

--- .ci/build-linux-arm.sh
+++ .ci/build-linux-arm.sh
@@ -2,10 +2,6 @@
 
 set -ex
 
-GO_MAXPROCS=(
-	GOMAXPROCS=1
-)
-
 GO_ENV=(
 	CGO_ENABLED=1
 )
@@ -23,6 +19,6 @@ OUTPUT_DIR=$(pwd)/linux-arm-binary
 export GOPATH=$(pwd)/gopath:$(pwd)/gopath/src/github.com/bchalk101/selfhydro/Godeps/_workspace
 cd gopath/src/github.com/bchalk101/selfhydro
 
-env ${GO_MAXPROCS[@]} go get
+env  go get
 env ${GO_CROSS_ENV[@]} go build -o "$OUTPUT_DIR/selfhydro"
 env ${GO_ENV[@]} go test ./... --cover -v
