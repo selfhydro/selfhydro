a424786a7703aa7d3f4d3cb6102ea80f5e0ca2f4
bchalk101 <boruch11@gmail.com>
2018-03-02 14:58:09 +1100

    Limit threads to one at a time for go get

--- .ci/build-linux-arm.sh
+++ .ci/build-linux-arm.sh
@@ -4,6 +4,7 @@ set -ex
 
 GO_ENV=(
 	CGO_ENABLED=1
+	GOMAXPROCS=1
 )
 
 GO_CROSS_ENV=(
@@ -11,6 +12,7 @@ GO_CROSS_ENV=(
 	GOARM=7
 	GOOS=linux
 	CGO_ENABLED=1
+	GOMAXPROCS=1
 )
 
 OUTPUT_DIR=$(pwd)/linux-arm-binary
@@ -18,6 +20,6 @@ OUTPUT_DIR=$(pwd)/linux-arm-binary
 export GOPATH=$(pwd)/gopath:$(pwd)/gopath/src/github.com/bchalk101/selfhydro/Godeps/_workspace
 cd gopath/src/github.com/bchalk101/selfhydro
 
-env go get
+env ${GO_ENV[@]} go get
 env ${GO_CROSS_ENV[@]} go build -o "$OUTPUT_DIR/selfhydro"
 env ${GO_ENV[@]} go test ./... --cover -v
