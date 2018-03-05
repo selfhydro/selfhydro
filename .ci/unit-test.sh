2a800faa25136e3731dbc9f42afa2eb2e643ffea
bchalk101 <boruch11@gmail.com>
2018-03-05 11:22:38 +1100

    Change unit test script

--- .ci/unit-test.sh
+++ .ci/unit-test.sh
@@ -8,9 +8,7 @@ mkdir -p src/github.com/bchalk101/
 cp -R ./selfhydro src/github.com/bchalk101/.
 
 cd src/github.com/bchalk101/selfhydro
-ls -lat
-
-go get ./...
+go get
 go test -cover ./... | tee test_coverage.txt
 
 mv test_coverage.txt $GOPATH/coverage-results/.
