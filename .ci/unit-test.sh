41870cb6a0d1d0e85ce2363a9d25feb3e3ae37a4
Boruch Chalk <boruch11@gmail.com>
2018-03-06 14:54:25 +1100

    Uncomment all

--- .ci/unit-test.sh
+++ .ci/unit-test.sh
@@ -1,15 +1,15 @@
-##!/bin/bash
+#!/bin/bash
 
-#set -e
-#
-#export GOPATH=$PWD
-#
-#mkdir -p src/github.com/bchalk101/
-#cp -R ./selfhydro src/github.com/bchalk101/.
-#
-#cd src/github.com/bchalk101/selfhydro
-#go get
-#go test -cover ./... | tee test_coverage.txt
-#
-#mv test_coverage.txt $GOPATH/coverage-results/.
-#
+set -e
+
+export GOPATH=$PWD
+
+mkdir -p src/github.com/bchalk101/
+cp -R ./selfhydro src/github.com/bchalk101/.
+
+cd src/github.com/bchalk101/selfhydro
+go get
+go test -cover ./... | tee test_coverage.txt
+
+mv test_coverage.txt $GOPATH/coverage-results/.
+
