cf9a11755c9bb521abf6a35583063417fa6cde78
Boruch Chalk <boruch11@gmail.com>
2018-03-07 21:26:58 +1100

    Add stage for release, versioning and docker

--- .ci/build-linux-arm.sh
+++ .ci/build-linux-arm.sh
@@ -26,16 +26,14 @@ cp -R ./selfhydro src/github.com/bchalk101/.
 cd src/github.com/bchalk101/selfhydro
 
 go get
-env ${GO_CROSS_ENV[@]} go build -o binary/selfhydro
+env ${GO_CROSS_ENV[@]} go build -o release/selfhydro
 
 ls -la
 
-cd binary
+echo "v$(cat version/version)" > release/name
+echo "v$(cat version/version)" > release/tag
 
-#docker build .
-
-scp selfhydro pi@10.1.1.6:/selfhydro/
-
-#ssh pi@10.1.1.6 'docker kill selfhydro'
-#ssh pi@10.1.1.6 'docker run -v /sys:/sys -v /selfhydro:/selfhydro selfhydro'
+cat > release/body <<EOF
+Selfhydro compiled to be placed in docker image
+EOF
 
