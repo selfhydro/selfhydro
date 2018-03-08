ba3b1f130bf22e6563197b27020b9c91c55effa1
bchalk101 <boruch11@gmail.com>
2018-03-08 14:02:32 +1100

    Try empty scrpit

--- .ci/publish-docker-image.sh
+++ .ci/publish-docker-image.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-TAG=$(cat selfhydro-release/tag)
-
-docker build --tag selfhydro-release:${TAG} /selfhydro/.ci/docker/
-
-docker login -u ${DOCKER_USERNAME} -p ${DOCKER_PASSWORD}
-
-docker push selfhydro-release:${TAG}
+#TAG=$(cat selfhydro-release/tag)
+#
+#docker build --tag selfhydro-release:${TAG} /selfhydro/.ci/docker/
+#
+#docker login -u ${DOCKER_USERNAME} -p ${DOCKER_PASSWORD}
+#
+#docker push selfhydro-release:${TAG}
