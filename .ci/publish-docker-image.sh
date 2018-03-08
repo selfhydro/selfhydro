358b19acdd35ba4b03512e5317714792e8b398f4
bchalk101 <boruch11@gmail.com>
2018-03-08 19:38:45 +1100

    Get docker using curl

--- .ci/publish-docker-image.sh
+++ .ci/publish-docker-image.sh
@@ -1,9 +1,32 @@
 #!/bin/bash
 
-apt-get update
-apt-get install -y curl
+set -e -x
 
-apt-get install docker-ce
+apt-get update
+
+apt-get install -y curl
+#
+#apt-get install \
+#     apt-transport-https \
+#     ca-certificates \
+#     curl \
+#     gnupg2 \
+#     software-properties-common
+
+#curl -fsSL https://download.docker.com/linux/$(. /etc/os-release; echo "$ID")/gpg | sudo apt-key add -
+#
+#sudo apt-key fingerprint 0EBFCD88
+#
+#add-apt-repository \
+#   "deb [arch=amd64] https://download.docker.com/linux/$(. /etc/os-release; echo "$ID") \
+#   $(lsb_release -cs) \
+#   stable"
+#
+#apt-get install -y docker-ce
+
+curl -fsSL get.docker.com -o get-docker.sh
+sh get-docker.sh
+
 docker run hello-world
 
 TAG=$(cat selfhydro-release/tag)
