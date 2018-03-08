a9323ddea50026c8996ab5417572642179f77ebd
bchalk101 <boruch11@gmail.com>
2018-03-08 19:24:52 +1100

    Use task to compile docker image

--- .ci/publish-docker-image.sh
+++ .ci/publish-docker-image.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-echo "Hello"
+apt-get update
+apt-get install -y curl
 
-#TAG=$(cat selfhydro-release/tag)
-#
-#docker build --tag selfhydro-release:${TAG} /selfhydro/.ci/docker/
-#
-#docker login -u ${DOCKER_USERNAME} -p ${DOCKER_PASSWORD}
-#
-#docker push selfhydro-release:${TAG}
+apt-get install docker-ce
+docker run hello-world
+
+TAG=$(cat selfhydro-release/tag)
+
+docker build --tag selfhydro-release:${TAG} /selfhydro/.ci/docker/
+
+docker login -u ${DOCKER_USERNAME} -p ${DOCKER_PASSWORD}
+
+docker push selfhydro-release:${TAG}
