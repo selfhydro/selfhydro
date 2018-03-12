d71a3b6e364434ba7b79d00f44fb159968e7a5d7
bchalk101 <boruch11@gmail.com>
2018-03-12 14:13:37 +1100

    Add deploy key for raspberry pi

--- .ci/deploy.sh
+++ .ci/deploy.sh
@@ -2,7 +2,17 @@
 
 set -e -x
 
+echo $DEPLOY_KEY \
+  | sed -e 's/\(KEY-----\)\s/\1\n/g; s/\s\(-----END\)/\n\1/g' \
+  | sed -e '2s/\s\+/\n/g' > deploy_key
+
+unset DEPLOY_KEY
+
+set -ex
+
+chmod 600 deploy_key
+
 TAG=$(cat selfhydro-release/tag)
 
-ssh -o StrictHostKeyChecking=no pi@10.1.1.6 'docker rm -f selfhydro || true'
-ssh -o StrictHostKeyChecking=no pi@10.1.1.6 'docker run --name selfhydro --restart=always -v /sys:/sys -v /selfhydro:/selfhydro bchalk/selfhydro:${TAG}'
+ssh -o StrictHostKeyChecking=no -i deploy_key pi@10.1.1.6 'docker rm -f selfhydro || true'
+ssh -o StrictHostKeyChecking=no -i deploy_key pi@10.1.1.6 'docker run --name selfhydro --restart=always -v /sys:/sys -v /selfhydro:/selfhydro bchalk/selfhydro:${TAG}'
