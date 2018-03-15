1afa75dbc1a7ddca0b4eed5abcff03ecfbcce2ad
bchalk101 <boruch11@gmail.com>
2018-03-15 13:20:01 +1100

    Copy selfhydro binary to pi

--- .ci/deploy.sh
+++ .ci/deploy.sh
@@ -14,8 +14,9 @@ chmod 600 deploy_key
 
 TAG=$(cat selfhydro-release/tag)
 
+scp -o StrictHostKeyChecking=no -i deploy_key  /selfhydro-release/selfhydro pi@10.1.1.2:/selfhydro/
 ssh -o StrictHostKeyChecking=no -i deploy_key  pi@10.1.1.2 'sudo pkill -f -o selfhydro'
-ssh -o StrictHostKeyChecking=no -i deploy_key  pi@10.1.1.2 'nohup sudo /selfhydro-release/selfhydro &'
+ssh -o StrictHostKeyChecking=no -i deploy_key  pi@10.1.1.2 'nohup sudo /selfhydro/selfhydro &'
 
 #ssh -o StrictHostKeyChecking=no  pi@10.1.1.2 'docker rm -f selfhydro || true '
 #ssh -o StrictHostKeyChecking=no  pi@10.1.1.2  "docker run --name selfhydro --restart=always --privileged=true -v /selfhydro:/selfhydro bchalk/selfhydro-release:$TAG"
