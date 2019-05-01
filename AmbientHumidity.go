26b54893c8b6d1eee114b99389ff9354fb70a1ae
Baz <boruch11@gmail.com>
2019-05-01 18:28:33 +1000

    Get ambient humidity state and send to gcp mqtt

---
+++ AmbientHumidity.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+
+	"github.com/bchalk101/selfhydro/mqtt"
+	mqttPaho "github.com/eclipse/paho.mqtt.golang"
+)
+
+type humidityMessage struct {
+	Humidity float64 `json:"humidity"`
+}
+
+type AmbientHumidity struct {
+	humidity float64
+}
+
+const AmbientHumidityTopic = "/state/ambient_humidity"
+
+func (e *AmbientHumidity) Subscribe(mqtt mqtt.MQTTComms) error {
+	if err := mqtt.SubscribeToTopic(AmbientHumidityTopic, e.HumidityHandler); err != nil {
+		log.Print(err.Error())
+		return err
+	}
+	return nil
+}
+
+func (e *AmbientHumidity) HumidityHandler(client mqttPaho.Client, message mqttPaho.Message) {
+	humidity := &humidityMessage{}
+	json.Unmarshal(message.Payload(), humidity)
+	e.humidity = humidity.Humidity
+}
+
+func (e AmbientHumidity) GetLatestData() float64 {
+	return e.humidity
+}
