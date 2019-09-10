5d1a894e449997a87076326421604c02e3b16420
Baz <boruch11@gmail.com>
2019-09-10 20:38:43 +1000

    add flags to build

--- db/.ci/build.sh
+++ db/.ci/build.sh
@@ -8,7 +8,7 @@ apt-get install zip -y
 VERSION=$(cat version/version)
 
 cd ./selfhydro/db/
-GOOS=linux go build -o dynamoDBTableCreater
+GOOS=linux go build -v -ldflags '-d -s -w' -o dynamoDBTableCreater
 
 
 zip selfhydro-state-db-release.zip dynamoDBTableCreater
