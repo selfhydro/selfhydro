a409b35c7a78eb6a4bbc97f245c5373e39eecb38
Baz <boruch11@gmail.com>
2019-09-11 09:00:27 +1000

    add hash for file

--- db/infra/main.tf
+++ db/infra/main.tf
@@ -96,6 +96,7 @@ resource "aws_lambda_function" "create_dynamo_db_tables" {
   role          = "${aws_iam_role.iam_for_lambda.arn}"
   handler       = "dynamoDBTableCreater"
   runtime       = "go1.x"
+  source_code_hash = filebase64sha256("selfhydro-state-db/selfhydro-state-db-release.zip")
 
   environment {
     variables = {
